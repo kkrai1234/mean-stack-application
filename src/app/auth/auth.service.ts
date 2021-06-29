a1915c31845f9bad03ed02988fdda2ab345efdf2
kkrai1234 <kkrai1234@gmail.com>
2021-06-29 23:55:01 +0530

    Add files via upload

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+import { AuthData } from "./auth-data.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  token: string;
+  private isAuthenticated = false;
+  private authStatusListener =new Subject<boolean>();
+  constructor(private http: HttpClient){
+
+  }
+  getToken(){
+    return this.token;
+  }
+  getIsAuth(){
+    return this.isAuthenticated;
+  }
+  getAuthStatusListener(){
+    return this.authStatusListener.asObservable();
+  }
+  createUser(email: string, password: string){
+    const authData: AuthData = {email: email, password: password};
+    this.http.post("http://localhost:3000/user/signup",authData)
+    .subscribe(response=>{
+      console.log(response);
+    })
+  }
+
+  login(email: string, password: string){
+    const authData: AuthData = {email: email, password: password};
+    this.http.post<{token: string}>("http://localhost:3000/user/login",authData)
+    .subscribe(response=>{
+      const token = response.token;
+      this.token = token;
+      if(token){
+        this.isAuthenticated = true;
+        this.authStatusListener.next(true);
+      }
+    })
+  }
+}
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { AuthService } from "../auth/auth.service";
+
+@Component
+({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit, OnDestroy {
+  private authListenerSubs: Subscription;
+  userIsAuthenticated = false;
+  constructor(private authService: AuthService){
+
+  }
+  ngOnInit(){
+this.authListenerSubs = this.authService.getAuthStatusListener()
+.subscribe(isAuthenticated =>{
+  this.userIsAuthenticated = isAuthenticated;
+})
+  }
+  ngOnDestroy(){
+
+  }
+}
